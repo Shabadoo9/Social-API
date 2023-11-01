f4a5d4ae514e82d63c1773fcbb681ac4a28a6d82
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-01 01:21:34 -0400

    completed thought.js model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function (date) {
+                return new Date(date).toLocaleString();
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        _id: false,
+        timestamps: { createdAt: true, updatedAt: false },
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
