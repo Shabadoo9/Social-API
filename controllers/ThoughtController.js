d65035ea138ca5ec41ffd92bcdeee2e6f70dc2e9
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-01 01:57:58 -0400

    added updateThought & addthought

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -23,3 +23,40 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    async addThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id } },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'Body must include a valid user id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: { thoughtText: req.body.thoughtText } },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
