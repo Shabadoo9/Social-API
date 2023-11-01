b91b6687dbc6dd70f8012cf00c44e432b35b6fb0
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-01 01:39:11 -0400

    created controller files

---
+++ controllers/ThoughtController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
